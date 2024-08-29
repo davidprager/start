cc24588fe6fc0d8639e6195aaafb88ba439e14dd
davidprager <84202740+davidprager@users.noreply.github.com>
2024-08-29 19:22:03 -0400

    Add files via upload

---
+++ start.py
@@ -0,0 +1,11 @@
+"""
+CSCI-UA.0060 Spring 2024
+Test Program
+"""
+# This program will make sure that you are able to edit a simple program in GitHub
+# Update the program to include your name instead of David and your GitHub username.
+# Once done, commit the changes
+
+
+print ("Hi there, my name is David")
+print ("My GitHub username is -------")
