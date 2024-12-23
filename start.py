26f45586fc09fe9046d7b0959f59aaa7eaa3524e
davidprager <84202740+davidprager@users.noreply.github.com>
2024-12-22 23:25:17 -0500

    Update start.py

--- start.py
+++ start.py
@@ -3,9 +3,9 @@ CSCI-UA.0060 Spring 2024
 Test Program
 """
 # This program will make sure that you are able to edit a simple program in GitHub
-# Update the program to include your name instead of David and your GitHub username.
+# Update the program to replace my name with your first and last name and add in your GitHub username.
 # Once done, commit the changes
 
 
-print ("Hi there, my name is David")
+print ("Hi there, my name is David Prager")
 print ("My GitHub username is -------")
